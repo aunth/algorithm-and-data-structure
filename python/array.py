f9351f51ca409df1ee389fa7d097a1ee85c45eb9
Vlad Maslianko <vladmaslianko@MacBook-Air-Vlad.local>
2023-04-11 21:21:40 +0300

    Delete unnecessary functions

--- python/array.py
+++ python/array.py
@@ -2,71 +2,52 @@
 class MyArray:
     def __init__(self) -> None:
         self.data = {}
-        self.lenght = 0
+        self.length = 0
 
     def append(self, value):
-        self.data[self.lenght] = value
-        self.lenght += 1
+        self.data[self.length] = value
+        self.length += 1
 
     def get(self, index):
-        if index < self.lenght:
+        if index < self.length:
             return self.data[index]
         
     def pop(self):
-        last_item = self.data[self.lenght - 1]
-        del self.data[self.lenght - 1]
-        self.lenght -= 1
+        last_item = self.data[self.length - 1]
+        del self.data[self.length - 1]
+        self.length -= 1
         return last_item
     
     def delele(self, index):
-        if index < self.lenght:
-            for i in range(index, self.lenght-1):
+        if index < self.length:
+            for i in range(index, self.length-1):
                 self.data[i] = self.data[i+1]
-        del self.data[self.lenght-1]
-        self.lenght -= 1
+        del self.data[self.length-1]
+        self.length -= 1
         print(self.data)
     
     def insert(self, index, value):
-        self.lenght += 1
-        print(self.lenght)
-        for i in range(self.lenght - 1, index, -1):
+        self.length += 1
+        print(self.length)
+        print(self.data)
+        print(index)
+        for i in range(self.length - 1, index, -1):
             self.data[i] = self.data[i - 1]
         self.data[index] = value
         print(self.data)
 
-def selection_sort(array):
-    lenght = len(array)
-    for i in range(lenght):
-        for j in range(i+1, lenght):
-            if array[i] > array[j]:
-                array[j], array[i] = array[i], array[j]
-    print(array)
-
-def selection_sort_recutsion(array, start, end):
-    if end == start:
-        return
-    for i in range(end, start, -1):
-        if array[i] > array[end]:
-            array[end], array[i] = array[i], array[end]
-    print(array)
-    return selection_sort_recutsion(array, start, end-1)
-
-
-class MyStruct: #To implement data structure like dynamic arrays but which have delete/insert elements at the start in O(1) time
-    def __init__(self): 
-        self.left = {}
-        self.right = {}
-        self.lenght
-
-    def append(self, value):
-        lenght = len(value)
-        for i in range(lenght//2):
-            self.left[i] = value[i]
-        
-        for i in range(lenght//2, lenght):
-            self.right[i] = value[i]
+    def print_array(self):
+        print("[ ", end='')
+        for i in range(self.length-1):
+            print(f"{self.data[i]}, ", end='')
+        print(f"{self.data[self.length-1]} ]")
 
 
 if __name__ == "__main__":
-    print(selection_sort_recutsion([2, 3, 1, 5, 8, 19, 12], -1, 6))
+    a = MyArray()
+    a.append(10)
+    a.append("Value")
+    a.insert(1, "hello world")
+    a.insert(1, "50")
+    a.print_array()
 
