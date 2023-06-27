00afa75078b1e666119df8477f3119a3346d90ee
vmaslian <vmaslian@student.42wolfsburg.de>
2023-06-28 00:05:37 +0300

    Add selection sort

---
+++ algorithms/sorting/selection_sort.py
@@ -0,0 +1,15 @@
+import sorting_test
+
+def selection_sort(array):
+    for i in range(len(array)):
+        min_index = i
+        for j in range(i+1, len(array)):
+            if array[min_index] > array[j]:
+                min_index = j
+        array[i], array[min_index] = array[min_index], array[i]
+    return array
+
+def main():
+    sorting_test.tests(selection_sort, 1000)
+    
+main()
